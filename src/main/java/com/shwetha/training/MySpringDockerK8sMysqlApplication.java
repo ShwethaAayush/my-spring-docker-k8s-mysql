4f115f94de0d50e2b2c4ed99a52621b88c35351a
ShwethaAayush <ShwethaAayush@github.com>
2020-12-18 18:54:39 +0530

    first commit with kubes

---
+++ src/main/java/com/shwetha/training/MySpringDockerK8sMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.shwetha.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringDockerK8sMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringDockerK8sMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shwetha/training/MySpringDockerK8sMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shwetha.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringDockerK8sMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
